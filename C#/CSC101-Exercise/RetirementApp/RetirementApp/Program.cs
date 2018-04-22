c16a8b1d4eb723c21bdb13abc89717fad9c90a0e
rogeriofabricio <rogerio.fabricio@hotmail.com>
2018-04-22 19:03:42 -0300

    csharp-101
    Conceitos básicos

---
+++ C#/CSC101-Exercise/RetirementApp/RetirementApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetirementApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int retirement = 65;
+
+            int myAge = 25;
+            int yourAge = 35;
+
+            string message = "Will we be ready to retire soon?";
+
+            int yearsToRetirementForYou = retirement - yourAge;
+            int yearsToRetirementForMe = retirement - myAge;
+
+            Console.WriteLine(message);
+
+            Console.WriteLine("Years for you: " + yearsToRetirementForYou);
+            Console.WriteLine("Years for me: {0}", yearsToRetirementForMe);
+
+        }
+    }
+}
