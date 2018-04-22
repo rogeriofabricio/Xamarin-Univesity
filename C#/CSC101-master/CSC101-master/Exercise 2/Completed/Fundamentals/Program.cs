c30c0b7d3dfe1547418f65bf28137703e511f3a2
rogeriofabricio <rogerio.fabricio@hotmail.com>
2018-04-22 18:45:49 -0300

    Initial commit

---
+++ C#/CSC101-master/CSC101-master/Exercise 2/Completed/Fundamentals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fundamentals
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int myAge   = 22;
+			int yourAge = 35;
+
+			const int retirement = 65;
+
+			string message = "Will we be ready to retire soon?";
+
+			myAge = 59; // variable can change its value
+
+			/*
+			 * Use subtraction to calculate years-to-retirement
+			 */
+			int yearsToRetirementForYou = retirement - yourAge;
+			int yearsToRetirementForMe  = retirement - myAge;
+
+			/*
+			 * Print the value of a variable
+			 */
+			Console.WriteLine(message);
+
+			Console.WriteLine("Years for you: " + yearsToRetirementForYou); // concatenating strings
+			Console.WriteLine("Years for me: {0}", yearsToRetirementForMe); // formatting strings
+		}
+	}
+}
---
+++ C#/CSC101-master/CSC101-master/Homework/Completed/BillCalculation/BillCalculation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BillCalculation
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//
+			// Symbolic constants
+			//
+			const double SodaPrice     = 1.50;
+			const double SandwichPrice = 4.50;
+			const double ChipsPrice    = 1.25;
+			const double TaxRate       = 0.08;
+			const double TipRate       = 0.15;
+
+			//
+			// Items purchased by customer
+			//
+			int numSodas      = 2;
+			int numSandwiches = 2;
+			int numChips      = 1;
+
+			//
+			// Calculations
+			//
+			double sodaTotal     = numSodas      * SodaPrice;
+			double sandwichTotal = numSandwiches * SandwichPrice;
+			double chipsTotal    = numChips      * ChipsPrice;
+
+			double subtotal = sodaTotal + sandwichTotal + chipsTotal;
+
+			double tax = subtotal * TaxRate;
+			double tip = subtotal * TipRate;
+
+			double total = subtotal + tax + tip;
+
+			//
+			// Output: the "C" in the format specification "{0:C}" stands for "currency"
+			// and will include your local currency symbol in the printed output.
+			//
+			Console.WriteLine("Subtotal {0:C}", subtotal);
+			Console.WriteLine("Tax      {0:C}", tax);
+			Console.WriteLine("Tip      {0:C}", tip);
+			Console.WriteLine("TOTAL    {0:C}", total);
+		}
+	}
+}
